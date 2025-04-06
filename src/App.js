2d49ce746c435c9f344ae83307bd45aa1b518b3e
Hyun Joon Oh <sinra1010@naver.com>
2025-04-06 19:21:42 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import Customer from './components/Customer';
 import './App.css';
+import Customer from './components/Customer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 const customers = [
   {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '오현준',
-    'birthday': '001211',
-    'gender': '남자',
-    'job': '취준생'
+    id: 1,
+    image: 'https://picsum.photos/64/64?random=1',
+    name: '오현준',
+    birthday: '001211',
+    gender: '남자',
+    job: '취준생'
   },
   {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '스티븐잡스',
-    'birthday': '951130',
-    'gender': '남자',
-    'job': 'CEO'
+    id: 2,
+    image: 'https://picsum.photos/64/64?random=2',
+    name: '스티븐잡스',
+    birthday: '951130',
+    gender: '남자',
+    job: 'CEO'
   },
   {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '일론머스크',
-    'birthday': '901030',
-    'gender': '남자',
-    'job': '디자이너'
+    id: 3,
+    image: 'https://picsum.photos/64/64?random=3',
+    name: '일론머스크',
+    birthday: '901030',
+    gender: '남자',
+    job: '디자이너'
   }
-]
+];
+
 function App() {
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
+    <Paper sx={{ width: '100%', mt: 3, overflowX: 'auto' }}>
+      <Table sx={{ minWidth: 1080 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((c) => (
             <Customer
               key={c.id}
               id={c.id}
@@ -40,11 +57,12 @@ function App() {
               name={c.name}
               birthday={c.birthday}
               gender={c.gender}
+              job={c.job}
             />
-          )
-        })
-      }
-    </div>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
