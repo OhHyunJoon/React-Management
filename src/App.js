8d5f60ae5257ff2dc68ad357082613daa36827ef
Hyun Joon Oh <sinra1010@naver.com>
2025-04-06 16:11:31 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '오현준',
+    'birthday': '001211',
+    'gender': '남자',
+    'job': '취준생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '스티븐잡스',
+    'birthday': '951130',
+    'gender': '남자',
+    'job': 'CEO'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '일론머스크',
+    'birthday': '901030',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
 function App() {
   return (
-    <div className="App">
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+            />
+          )
+        })
+      }
     </div>
   );
 }
